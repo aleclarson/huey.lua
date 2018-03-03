cd254f9c1f100cece29312868517c2f172ae230e
aleclarson <alec.stanford.larson@gmail.com>
2018-03-03 18:17:20 -0500

    feat: add `rgb` function

--- huey.lua
+++ huey.lua
@@ -1,14 +1,23 @@
 
-function install(print)
-  local esc = string.char(27)
-  local function fg(n)
-    return '%s[38;5;%dm':format(esc, n)
-  end
-  local function rgb(r, g, b)
-    return 16 + 36*r + 6*g + b
-  end
+local esc = string.char(27)
+local fg_reset = esc..'[39m'
 
-  local fg_reset = esc..'[39m'
+local function fg(n)
+  return '%s[38;5;%dm':format(esc, n)
+end
+
+local function rgb(r, g, b)
+  return 16 + 36*r + 6*g + b
+end
+
+local function rgb_fn(r, g, b)
+  local color = fg(rgb(r, g, b))
+  return function(str)
+    return color .. str .. fg_reset
+  end
+end
+
+local function install(print)
   local fg_colors = {
     red = {5, 0, 0},
     blue = {0, 1, 5},
@@ -34,6 +43,7 @@ function install(print)
 end
 
 return {
+  rgb = rgb_fn,
   install = install,
 }
 
